66cd6243d7c8c29e74c37fe6a47a296fe5c26c67
Adhishthatri <asunasingh2003@gmail.com>
2025-08-22 12:06:02 +0530

    page.tsx so far

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,32 @@
-import Image from "next/image";
+"use client";
+
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const emotions = [
+    { value: 0, label: "😔 Down", color: "from-blue-500/50" },
+    { value: 25, label: "😊 Content", color: "from-green-500/50" },
+    { value: 50, label: "😌 Peaceful", color: "from-purple-500/50" },
+    { value: 75, label: "🤗 Happy", color: "from-yellow-500/50" },
+    { value: 100, label: "✨ Excited", color: "from-pink-500/50" },
+  ];
+
+  
+  const [emotion, setEmotion] = useState(50);
+  const [mounted, setMounted] = useState(false);
+  const [showDialog, setShowDialog] = useState(false);
+  const [currentStep, setCurrentStep] = useState(0);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const currentEmotion =
+    emotions.find((em) => Math.abs(emotion - em.value) < 15) || emotions[2];
+
+    
   return (
     <div className="flex flex-col min-h-screen overflow-hidden ">
       
