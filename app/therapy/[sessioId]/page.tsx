ea2d4fd8c36da05390610eb471bc78eba81eed93
Adhishthatri <asunasingh2003@gmail.com>
2025-09-03 10:32:26 +0530

    Initial setup and core library imports

---
+++ app/therapy/[sessioId]/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import {
+  Send,
+  Bot,
+  User,
+  Loader2,
+  Sparkles,
+  X,
+  Trophy,
+  Star,
+  Clock,
+  Smile,
+  PlusCircle,
+  MessageSquare,
+} from "lucide-react";
+
